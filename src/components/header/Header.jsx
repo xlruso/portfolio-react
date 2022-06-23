04fc5b18b640f353b812ca9be6843b6000dc9414
facundo <facundo.barbeito17@gmail.com>
2022-06-23 19:12:14 -0300

    prueba

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
+const header = () => {
+  return (
+    <header>
+      <section id='header' className="container header__container">
+        <h5>Hola, Soy</h5>
+        <h1>Facundo Barbeito</h1>
+        <h5 className="text-light">Front End Developer</h5>
+        <CTA />
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Desplaza hacia abajo</a>
+      </section>
+    </header>
+  )
+}
+
+export default header
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiProjectorLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+
+const Nav = () => {
+  return (
+    <nav>
+      <a href="#header" className='active'><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><RiProjectorLine/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
+  )
+}
+
+export default Nav
